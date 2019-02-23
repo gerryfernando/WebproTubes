f2669ccb1362aecf3ab44d63f88b7c7c0fba9f27
dingkai031 <yovanjulioadam@gmail.com>
2019-02-23 10:59:25 +0700

    berofe first push

---
+++ JS/beranda.js
@@ -0,0 +1,10 @@
+function opensm() {
+    document.getElementById("mysidemenu").style.width = "300px";
+    document.getElementById("bukamenu").style.display = "none";
+}
+
+function closesm() {
+    document.getElementById("mysidemenu").style.width = "0px";
+    document.getElementById("bukamenu").style.display = "block";
+}
+
